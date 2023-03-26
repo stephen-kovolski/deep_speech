40516d3ef0e1cec8c4175fd0cb5fa919e3760c81
stephen-kovolski <s.kovolski@gmail.com>
2023-03-25 23:15:26 -0400

    trying to add microphone access

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
+const texts = document.querySelector('.texts');
+
+
 //Below is required.  This is feeding the right objects to chrome
-const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 
 
 //Below is creating a new instance of the speech recognition. 
 //we use the SpeechRecognition constructor and create a new instance.
-let recognition = new SpeechRecognition();
+const recognition = new window.SpeechRecognition();
+
+//THe below line allows the program to print speech as the user says it.  
+//If it was set to false, the program would wait until we finished talking to print everything we said.
+recognition.interimResults = true;
+
 
 //if you have certain grammer restrictions, you could put them here.
 /*******const speechRecognitionList = new SpeechGrammarList();*********/
+
+
+let p = document.createElement('p');
+
+recognition.addEventListener('result', (e) => {
+    console.log(e)
+})
+
+recognition.start();
+
