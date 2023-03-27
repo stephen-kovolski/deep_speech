7ef59c7eeced289890eb437a7e5a63e92808ea53
stephen-kovolski <s.kovolski@gmail.com>
2023-03-26 21:39:12 -0400

    Mic access works, getting my speech in the console

--- main.js
+++ main.js
@@ -22,7 +22,25 @@ recognition.interimResults = true;
 let p = document.createElement('p');
 
 recognition.addEventListener('result', (e) => {
-    console.log(e)
+
+    const text = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join("");
+
+    p.innerText = text;
+    texts.appendChild(p);
+
+    if(e.results[0].isFinal){
+        
+        p = document.createElement('p')
+    }
+
+    console.log(text)
+})
+
+recognition.addEventListener('end', ()=>{
+    recognition.start();
 })
 
 recognition.start();
