f5bdf64c04e437feba982a938bb9bf169768cc15
stephen-kovolski <s.kovolski@gmail.com>
2023-03-27 08:25:29 -0400

    trying to put show soeech in gui

--- main.js
+++ main.js
@@ -1,47 +1,82 @@
-const texts = document.querySelector('.texts');
+// const texts = document.querySelector('.texts');
 
 
-//Below is required.  This is feeding the right objects to chrome
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+// //Below is required.  This is feeding the right objects to chrome
+// window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 
 
-//Below is creating a new instance of the speech recognition. 
-//we use the SpeechRecognition constructor and create a new instance.
-const recognition = new window.SpeechRecognition();
+// //Below is creating a new instance of the speech recognition. 
+// //we use the SpeechRecognition constructor and create a new instance.
+// const recognition = new window.SpeechRecognition();
 
-//THe below line allows the program to print speech as the user says it.  
-//If it was set to false, the program would wait until we finished talking to print everything we said.
+// //THe below line allows the program to print speech as the user says it.  
+// //If it was set to false, the program would wait until we finished talking to print everything we said.
+// recognition.interimResults = true;
+
+
+// //if you have certain grammer restrictions, you could put them here.
+// /*******const speechRecognitionList = new SpeechGrammarList();*********/
+
+
+// let p = document.createElement('p');
+
+// recognition.addEventListener('result', (e) => {
+
+//     const text = Array.from(e.results)
+//     .map(result => result[0])
+//     .map(result => result.transcript)
+//     .join("");
+
+//     p.innerText = text;
+//     texts.appendChild(p);
+
+//     if(e.results[0].isFinal){
+        
+//         p = document.createElement('p')
+//     }
+
+//     console.log(text)
+// })
+
+// recognition.addEventListener('end', ()=>{
+//     recognition.start();
+// })
+
+// recognition.start();
+
+
+const texts = document.querySelector(".texts");
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 
+let p = document.createElement("p");
 
-//if you have certain grammer restrictions, you could put them here.
-/*******const speechRecognitionList = new SpeechGrammarList();*********/
-
-
-let p = document.createElement('p');
-
-recognition.addEventListener('result', (e) => {
-
-    const text = Array.from(e.results)
-    .map(result => result[0])
-    .map(result => result.transcript)
+recognition.addEventListener("result", (e) => {
+  texts.appendChild(p);
+  const text = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
     .join("");
 
-    p.innerText = text;
-    texts.appendChild(p);
+  p.innerText = text;
+  texts.appendChild(p)
 
-    if(e.results[0].isFinal){
-        
-        p = document.createElement('p')
-    }
+  if(e.results[0].isFinal){
+    p=document.createElement('p');
+  }
+  
+    p = document.createElement("p");
+  
+});
 
-    console.log(text)
-})
-
-recognition.addEventListener('end', ()=>{
-    recognition.start();
-})
+recognition.addEventListener("end", () => {
+  recognition.start();
+});
 
 recognition.start();
 
